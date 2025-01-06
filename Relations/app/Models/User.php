9a44265de4ed5e6e9923108eec9b61c3711948ba
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-06 15:41:59 +0100

    Added Experience model, migration, and relationship to User model

--- Relations/app/Models/User.php
+++ Relations/app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
         return $this->hasOne(Profile::class);
     }
 
+    public function experience()
+    {
+        return $this->hasOne(Experience::class);
+    }
+
 }
