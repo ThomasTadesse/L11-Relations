3ccb24dfa25e86acd613967c0cb812cd5b658181
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-06 15:24:13 +0100

    profile table added

--- Relations/database/migrations/2025_01_06_141505_create_profiles_table.php
+++ Relations/database/migrations/2025_01_06_141505_create_profiles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('website_url');
+            $table->string('github_url');
+            $table->string('linkedin_url');
             $table->timestamps();
         });
     }
